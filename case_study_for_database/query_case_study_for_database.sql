28712b10e9dda5f6c894f848575ae206997c4e97
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-01-13 17:19:24 +0700

    uppp

--- case_study_for_database/query_case_study_for_database.sql
+++ case_study_for_database/query_case_study_for_database.sql
@@ -17,3 +17,14 @@ where loai_khach.ten_loai_khach like 'Diamond'
 group by khach_hang.ma_khach_hang
 order by count(hop_dong.ma_khach_hang);
 -- Task 5.Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ma_loai_khach, hop_dong.ma_hop_dong, 
+dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, 
+sum((dich_vu.chi_phi_thue + ifnull((hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia), 0))) as 'Tổng tiền'
+from hop_dong
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+right join khach_hang on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+inner join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+group by hop_dong.ma_hop_dong, khach_hang.ma_khach_hang
+order by khach_hang.ma_khach_hang;
